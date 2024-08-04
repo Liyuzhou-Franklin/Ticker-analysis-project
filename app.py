6f8880a840358138d9a54416180e78db93fd867f
Liyuzhou-Franklin <124677686+Liyuzhou-Franklin@users.noreply.github.com>
2024-08-04 23:25:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,52 +37,64 @@ def overall_sentiment(buy, hold, sell):
         return "hold"
 
 def get_stock_info(ticker):
-    url = f"https://seeking-alpha.p.rapidapi.com/symbols/get-chart"
     querystring = {"symbol": ticker}
+    querystring2 = {"id": ticker}
     headers = {
         "x-rapidapi-key": "6e90a4cc5amsh9324a55e6e118a5p1f97c2jsn28aa5a6e6a0a",
         "x-rapidapi-host": "seeking-alpha.p.rapidapi.com"
     }
+
+    # 1. Retrieving price information
+    url = f"https://seeking-alpha.p.rapidapi.com/symbols/get-chart"
     response = requests.request("GET", url, headers=headers, params=querystring)
     data = json.loads(response.text)
     date = get_last_workday()
     stock_data = data['attributes'][date]
 
+    # 2. Retrieving the ten latest Seeking Alpha articles analyzing the stock
     url2 = f"https://seeking-alpha.p.rapidapi.com/analysis/list"
-    querystring2 = {"id": ticker}
     response2 = requests.request("GET", url2, headers=headers, params=querystring2)
     data2 = response2.json()
     article_links = [article['links']['self'] for article in data2['data']]
     articles_list = []
     count = 1
-    while count <= 10:
+    for count in range(0, 10):
         for link in article_links:
             articles_list.append("seekingalpha.com" + link)
-        count += 1
 
+    # 3. Retrieving Seeking Alpha authors' buy/hold/sell opinions and calculating overall sentiment
     url3 = f"https://seeking-alpha.p.rapidapi.com/symbols/get-ratings"
-    querystring3 = {"symbol": ticker}
-    response3 = requests.request("GET", url3, headers=headers, params=querystring3)
+    response3 = requests.request("GET", url3, headers=headers, params=querystring)
     data3 = response3.json()
     item = data3['data'][0]
     attributes = item['attributes']
     ratings = attributes.get('ratings', {})
-    extracted_info = {
-        'Date': attributes.get('asDate'),
-        'Number of authors recommending buy': ratings.get('authorsRatingBuyCount'),
-        'Number of authors recommending hold': ratings.get('authorsRatingHoldCount'),
-        'Number of authors recommending sell': ratings.get('authorsRatingSellCount'),
-        'Overall sentiment': overall_sentiment(ratings.get('authorsRatingBuyCount'),
+    sentiment = {
+        'date': attributes.get('asDate'),
+        'num_buy': round(ratings.get('authorsRatingBuyCount')),
+        'num_hold': round(ratings.get('authorsRatingHoldCount')),
+        'num_sell': round(ratings.get('authorsRatingSellCount')),
+        'overall': overall_sentiment(ratings.get('authorsRatingBuyCount'),
                                                ratings.get('authorsRatingHoldCount'),
                                                ratings.get('authorsRatingSellCount'))
     }
-    sentiment = extracted_info
 
+    # 4. Retrieving the five latest earnings call transcripts
+    url4 = "https://seeking-alpha.p.rapidapi.com/transcripts/list"
+    response = requests.request("GET", url4, headers=headers, params=querystring2)
+    data4 = response.json()
+    transcript = []
+    for i in range(0, 5):
+        earnings = data4['data'][i]
+        link = earnings.get('links').get('self')
+        transcript.append("seekingalpha.com" + link)
+    
     return {
         "title": ticker,
         "price": stock_data,
         "articles": articles_list,
-        "sentiment": sentiment
+        "sentiment": sentiment,
+        "transcript": transcript
     }
 
 if __name__ == '__main__':
